1c5bcfb65b2dff191558705ffec2c0d1fad82895
Prith <prith98@gmail.com>
2022-01-25 12:26:37 -0800

    Created post and get functions for adding and getting from DB

--- server/index.js
+++ server/index.js
@@ -2,15 +2,25 @@ const axios = require("axios");
 const express = require("express");
 const path = require("path");
 const db = require('../database/index')
-const PORT = 3000;
+const PORT = 8080;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 
 
+app.post('/addTeam', db.addTeam);
 
-
+app.get('/getTeams', (req, res) => {
+  db.Trainer.find()
+    .then((results) => {
+      console.log('Successfully retrieved teams');
+      res.send(results);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+});
 
 
 app.listen(PORT, () => {
