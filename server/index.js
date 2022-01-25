12d5f9f03682fa76358ed41e21a5fba20cae8224
Prith <prith98@gmail.com>
2022-01-25 11:51:33 -0800

    Server up and running, gitignore and webpack created

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+const express = require("express");
+const path = require("path");
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`I'm listening on port ${PORT}`);
+});
