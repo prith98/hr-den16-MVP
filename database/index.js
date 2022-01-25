21aa74618489b96e6f2660b14b97c40f7441c2bc
Prith <prith98@gmail.com>
2022-01-25 12:21:45 -0800

    Created Trainer Schema and Model and addTeam function

--- database/index.js
+++ database/index.js
@@ -13,6 +13,42 @@ db.on('error', err => {
   console.error('connection error:', err)
 })
 
+const trainerSchema = new Schema({
+
+  trainer: {
+    type: String,
+    required: true
+  },
+  imgurl: String,
+  team: {
+    type: Schema.Types.Mixed,
+    required: true
+  }
+
+}, { timestamps: true});
+
+const Trainer = mongoose.model('Trainer', trainerSchema);
+
+const addTeam = (req, res) => {
+
+  let trainer = new Trainer({
+    "trainer": req.body.trainer,
+    "team": req.body.team,
+    "imgurl": req.body.imgurl
+  })
+  trainer.save()
+    .then((result) => {
+      console.log('Successfully added team')
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+
+}
+
 module.exports = {
   db,
+  Trainer,
+  addTeam,
 }
